63ea1761fd8e5194a3a0e9be5f0158c3be105ddf
Derek Wang <derekwangsz@gmail.com>
2022-06-09 07:51:10 -0700

    Initial Commit

---
+++ DubDubColours/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DubDubColours
+//
+//  Created by Derek Wang on 2022-06-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DubDubColours/DubDubColoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubColoursApp.swift
+//  DubDubColours
+//
+//  Created by Derek Wang on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct DubDubColoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
